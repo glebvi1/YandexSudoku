be3749cdcd58f321eaee89afa610210ff7c75b61
glebvi1 <vyazgd@mail.ru>
2021-10-17 01:11:48 +0300

    design

--- ui/Game.py
+++ ui/Game.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QLabel, QRadioButton
 
 from PyQt5 import uic
 
@@ -8,13 +8,26 @@ class Game(QWidget):
         super().__init__(parent)
         self.sudoku = sudoku
         uic.loadUi('ui/game.ui', self)
-        self.__draw_field()
 
-    def __draw_field(self):
+        self.__setup_ui()
+
+    def __setup_ui(self):
         for i in range(9):
             for j in range(9):
                 label = QLabel(self)
                 label.setObjectName(str(i) + str(j))
                 label.setText("0")
                 self.ui_field.addWidget(label, i, j)
+
+        x = 30
+        y = 500
+        for i in range(1, 10):
+            radio = QRadioButton(str(i), self)
+            radio.setObjectName("radio" + str(i))
+            radio.toggled.connect(self.__open_item)
+            radio.setGeometry(x, y, 30, 30)
+            x += 50
+
+    def __open_item(self):
+        pass
 
